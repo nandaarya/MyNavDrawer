cc7f61a55bbe10af7e4e094330a31bfcbb181eb7
Nanda Arya Putra <nandaarya404@gmail.com>
2023-11-02 23:37:01 +0700

    Add Menu Items

--- app/src/main/java/com/example/mynavdrawer/MyNavDrawerApp.kt
+++ app/src/main/java/com/example/mynavdrawer/MyNavDrawerApp.kt
@@ -6,6 +6,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -13,25 +16,48 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.mynavdrawer.ui.theme.MyNavDrawerTheme
 import kotlinx.coroutines.launch
 
+data class MenuItem(val title: String, val icon: ImageVector)
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyNavDrawerApp() {
     val drawerState = rememberDrawerState(DrawerValue.Closed)
     val scope = rememberCoroutineScope()
+
+    val items = listOf(
+        MenuItem(
+            title = stringResource(R.string.home),
+            icon = Icons.Default.Home
+        ),
+        MenuItem(
+            title = stringResource(R.string.favourite),
+            icon = Icons.Default.Favorite
+        ),
+        MenuItem(
+            title = stringResource(R.string.profile),
+            icon = Icons.Default.AccountCircle
+        ),
+    )
+
+    val selectedItem = remember { mutableStateOf(items[0]) }
 
     Scaffold(
         topBar = {
@@ -55,7 +81,17 @@ fun MyNavDrawerApp() {
             drawerContent = {
                 ModalDrawerSheet {
                     Spacer(Modifier.height(12.dp))
-                    Text(stringResource(R.string.hello_from_nav_drawer))
+                    items.forEach { item ->
+                        NavigationDrawerItem(
+                            icon = { Icon(item.icon, contentDescription = null) },
+                            label = { Text(item.title) },
+                            selected = item == selectedItem.value,
+                            onClick = {
+                                selectedItem.value = item
+                            },
+                            modifier = Modifier.padding(horizontal = 12.dp)
+                        )
+                    }
                 }
             },
             content = {
